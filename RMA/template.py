b24e50d028d197bc1db76917e5a3315bc69d6cd3
ManuelC96 <manuelchiocchetta@gmail.com>
2023-12-15 19:51:27 +0100

    commit

---
+++ RMA/template.py
@@ -0,0 +1,55 @@
+import docx as dx 
+from docx.shared import Mm
+# implement RMA aouto compiler
+# TODO TEMPLATE class
+
+class Template():
+    
+    def __init__(self, height= None, width= None, margins= None, header= None):
+        self.height = height
+        self.width = width
+        self.margin = margins
+        self.header = header
+#initialize worksheet 
+        self.document = dx.Document()
+        self.section = self.document.sections[0]
+        self.section.page_height = Mm(self.height)
+        self.section.page_width = Mm(self.width)
+        self.section.left_margin = Mm(self.margin)
+        self.section.right_margin = Mm(self.margin)
+        self.section.top_margin = Mm(self.margin)
+        self.section.bottom_margin = Mm(self.margin)
+        self.section.header_distance = Mm(self.header)
+        self.section.footer_distance = Mm(self.header) 
+# create title
+    def title(self, title = str, level = int):
+        self.title = title
+        self.level = level
+        self.document.add_heading(self.title, self.level)
+
+# # create table
+    def table(self, rows = None, cols = None):
+        self.rows = rows
+        self.cols = cols
+        return self.document.add_table(rows=self.rows, cols=self.cols)
+    
+# populate table 
+    def table_content(self, table = table, records= tuple):
+        self.records= recordsS
+        self.table = table
+    
+        for a in range(len(self.records)):
+            rowCells = self.table.add_row().cells
+
+            for j in range(len(self.records[a])):
+                
+                if self.records[a][j]:
+                    rowCells[j].text = str(self.records[a][j])
+                else:
+                    rowCells[j].text = input(f"inser value for:  {self.records[a][0]} ")
+# save the document
+    def save(self):
+        
+        document = self.document.save("demo.docx")
+        return None
+
---
+++ RMA/test.py
@@ -0,0 +1,44 @@
+from template import Template
+# create docx file
+document = Template(297, 210, 25.4, 12.7)
+document.title("Maintenance Fault Logging Template")
+table = document.table(rows= 1, cols= 2)
+tableRecords = (
+    ("Customer Information ",),
+    ("REQUESTED INFORMATION","DETAILS"),
+    ("Customer Name", "FASTWEB"),
+    ("End Customer Name","Fastweb VTS"),
+    ("Customer internal reference number:", 0),
+    ("Primary technical contact details:\nEngineer name, phone number, email", 0),
+    ("Severity /Priority Level: P1 / P2 / P3", 0),
+    ("","")
+        
+)
+
+table = document.table(rows= 1, cols= 2)
+tableRecords = (
+    ("Device serial number", 0),
+    ("Chassis Serial Number: ", 0),
+    ("Site name and address", 0),
+    ("Site contact details:\n (Customer - name, phone number, email)", 0)
+    ('''Site access: 
+    \n(Opening hours, please indicate onsite resource
+     availability if parts are delivered 
+     outside of site operating hours)''', 0),
+    ('''Additional Information: 
+    Including any special instructions for the 
+    on-site engineer when they arrive''', 0),
+    
+)
+
+
+
+
+
+
+
+document.table_content(table= table, records= tableRecords)
+
+
+# save the document
+document.save()    
